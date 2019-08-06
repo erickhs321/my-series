44d800c7cccef72ba19bc45b70ff9b13fc1f89d2
Erick <erickhenrique321@gmail.com>
2019-08-05 22:29:10 -0300

    proxy and request to api

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './Header';
+import axios from 'axios';
 import {
   BrowserRouter as Router,
   Route
@@ -15,12 +16,19 @@ const Generos = () => {
 
 
 function App() {
+  const [data, setData] = useState({})
+  useEffect(() =>{
+    axios.get('/api').then(res => {
+      setData(res.data)
+    })
+  }, [])
   return (
     <Router>
       <div>
         <Header />
         <Route path="/" exact component={Home} />   
         <Route path="/generos" component={Generos} />  
+        <pre>{JSON.stringify(data)}</pre>
       </div>
     </Router>
   );
