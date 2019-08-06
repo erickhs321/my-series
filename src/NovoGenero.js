630791e709ff833b2fc216776ebbe1dc0f67d791
Erick <erickhenrique321@gmail.com>
2019-08-05 23:49:36 -0300

    saving name in api and redirect to genres

--- src/NovoGenero.js
+++ src/NovoGenero.js
@@ -1,10 +1,25 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 const NovoGenero = () => {
-  const [name, setName] = useState('')
+  const [name, setName] = useState('');
+  const [success, setSuccess] = useState(false);
 
   const onChange = evt => {
     setName(evt.target.value)
+  };
+
+  const save = () => {
+    axios.post("/api/genres", {
+      name
+    })
+    .then(res => {
+      setSuccess(true)
+    })
+  }
+  if (success) {
+    return <Redirect to="/generos"/>
   }
 
   return ( 
@@ -14,7 +29,7 @@ const NovoGenero = () => {
         <div className="form-group">
           <label htmlFor="name">Nome</label>
           <input type="email" value={name} onChange={onChange} className="form-control" id="name" placeholder="Nome do Gênero"/>
-          <button type="button" className="btn btn-primary">Salvar</button>
+          <button type="button" onClick={save} className="btn btn-primary">Salvar</button>
         </div>
       </form>
     </div>
